d2c9048fd6badbc5d58f2b3a42d8ec36a938d5cb
Ana Tereza <anat.oliveira7@gmail.com>
2018-11-30 11:16:59 -0300

    database rodando e com tabelas users e bullets e migrations

---
+++ migrations/20181130110953_Datastructure.js
@@ -0,0 +1,40 @@
+
+exports.up = function(knex, Promise) {
+  return knex
+            .schema
+            .createTable( 'users', function( usersTable ) {
+                // Primary Key
+                usersTable.increments();
+
+                // Dados colunas
+                usersTable.string( 'username', 50 ).notNullable().unique();
+                usersTable.string( 'email', 250 ).notNullable().unique();
+                usersTable.string( 'password', 128 ).notNullable();
+                usersTable.string( 'guid', 50 ).notNullable().unique();
+
+            } )
+
+            .createTable( 'bullets', function( bulletsTable ) {
+
+                // Primary Key
+                bulletsTable.increments();
+                bulletsTable.string( 'owner', 36 ).references( 'guid' ).inTable( 'users' );
+
+                // Data
+                bulletsTable.string( 'descricao', 250 ).notNullable();
+                bulletsTable.string( 'categoria', 250 ).notNullable();
+                bulletsTable.datetime( 'vencimento');
+                bulletsTable.string( 'guid', 36 ).notNullable().unique();
+
+                bulletsTable.timestamp( 'criadoem' ).notNullable();
+
+            } );
+
+};
+
+exports.down = function(knex, Promise) {
+  return knex
+        .schema
+            .dropTableIfExists( 'bullets' )
+            .dropTableIfExists( 'users' );
+};
