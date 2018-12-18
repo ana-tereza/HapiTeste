24813ee5d3f31f8695dd1bef337f6894281ef680
Ana Tereza <anat.oliveira7@gmail.com>
2018-12-18 15:23:53 -0300

    add cadastrar usuario

--- src/routes/users.js
+++ src/routes/users.js
@@ -30,6 +30,48 @@ const info = request.params.user_info;
                 reply.response({ data: result[0] })
               );
     }
+  },
+{
+    method: "POST",
+    path: "/users",
+    handler: (request, reply) => {
+      try {
+        let { username, email, password } = JSON.parse(request.payload);
+        if (username === undefined) {
+          username = "";
+          return reply.response({ error: "undefined username" }).code(400);
+        }
+if (email === undefined) {
+          email = "";
+          return reply.response({ error: "undefined email" }).code(400);
+        }
+if (password === undefined) {
+          password = "";
+          return reply.response({ error: "undefined password" }).code(400);
+        }
+
+        const user = {
+          username: username,
+          email: email,
+          password: password
+        };
+        return knex
+          .into("users")
+          .insert(user)
+          .returning("username")
+          .then(result => {
+            user.oid = result[0];
+            return reply.response({ status: "cadastrado", data: user }).code(201);
+          })
+          .catch(err => {
+            return reply.response(err).code(400);
+          });
+      } catch (err) {
+        return reply
+          .response({ error: "undefined user in json object"})
+          .code(400);
+      }
+    }
   }
 ];
 
